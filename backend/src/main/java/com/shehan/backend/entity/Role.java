fc3e8f0cf4377a8e7bdab0cec08840e85b5ffc1b
Shehan <ertsshehan@gmail.com>
2026-06-10 15:18:45 +0530

    add Role and User entity classes with basic attributes and methods

---
+++ backend/src/main/java/com/shehan/backend/entity/Role.java
@@ -0,0 +1,5 @@
+package com.shehan.backend.entity;
+
+public enum Role {
+    USER, GUEST
+}
---
+++ backend/src/main/java/com/shehan/backend/entity/User.java
@@ -0,0 +1,45 @@
+package com.shehan.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
